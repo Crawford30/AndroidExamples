fb107546f5b3e093dad63f6bb3288317a44ce7c3
fouliex <fouliex@gmail.com>
2017-03-05 10:32:00 -0800

    Add Fragment example

---
+++ 3.AndroidFragments/AndroidFragment/app/src/main/java/com/fragment/example/fouliex/Fragment1.java
@@ -0,0 +1,21 @@
+package com.fragment.example.fouliex;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fragment.example.fouliex.androidfragment.R;
+
+/**
+ * Created by fouli on 3/5/2017.
+ */
+public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1,container, false);
+    }
+}
