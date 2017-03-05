6dae9b79ce516660585af1b4ae65b849d2e04680
fouliex <fouliex@gmail.com>
2017-03-05 08:37:47 -0800

    Add Android Toast Example

--- 1.AndroidWidgets/AndroidToast/app/src/main/java/com/example/toast/fouliex/androidtoast/MainActivity.java
+++ 1.AndroidWidgets/AndroidToast/app/src/main/java/com/example/toast/fouliex/androidtoast/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.toast.fouliex.androidtoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+/**
+ * Android Toast can be used to display information for a short or long period of time.
+ * Object <- Toast
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Displaying Toast with Hello World message
+        Toast.makeText(MainActivity.this,"Hello World",Toast.LENGTH_SHORT).show();
     }
 }
