2c744f884698b938658128fb49c48dfff96a176a
George Fouche <fouliex@gmail.com>
2017-03-10 14:53:38 -0800

    Add a Context Menu example

--- 4.AndroidMenu/ContextMenu/app/src/main/java/com/example/menu/context/fouliex/contextmenu/MainActivity.java
+++ 4.AndroidMenu/ContextMenu/app/src/main/java/com/example/menu/context/fouliex/contextmenu/MainActivity.java
@@ -2,12 +2,50 @@ package com.example.menu.context.fouliex.contextmenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+/**
+ * Android context menu appears when a user long press on a element. It is also know  as a floating
+ * menu and doesn't support item shortcuts and icons.
+ */
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String contacts[] = {"George", "Erol", "Stephane", "myriam"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contacts);
+        listView.setAdapter(adapter);
+        // Register the ListView for Context menu
+        registerForContextMenu(listView);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Select The Action");
+        menu.add(0, v.getId(), 0, "Call");//groupId, itemId, order, title
+        menu.add(0, v.getId(), 0, "SMS");
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        if(item.getTitle()=="Call"){
+            Toast.makeText(getApplicationContext(),"calling code",Toast.LENGTH_LONG).show();
+        }
+        else if(item.getTitle()=="SMS"){
+            Toast.makeText(getApplicationContext(),"sending sms code",Toast.LENGTH_LONG).show();
+        }else{
+            return false;
+        }
+        return true;
     }
 }
