f22d062a1d47488229ae7a1fb0424bce5a7d58fb
George Fouche <fouliex@gmail.com>
2017-03-10 15:03:50 -0800

    Android Service initial commit

---
+++ 5.AndroidService/AndroidService/app/src/main/java/com/example/service/fouliex/androidservice/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.service.fouliex.androidservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
