14c689b7d6d58a08ef5169a79d7a894eb0d5adf9
George Fouche <fouliex@gmail.com>
2017-03-10 13:59:00 -0800

    Add OptionMenu example

--- 4.AndroidMenu/OptionMenu/app/src/main/java/menucom/option/example/fouliex/optionmenu/MainActivity.java
+++ 4.AndroidMenu/OptionMenu/app/src/main/java/menucom/option/example/fouliex/optionmenu/MainActivity.java
@@ -2,12 +2,49 @@ package menucom.option.example.fouliex.optionmenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+/**
+ * Android Option Menus are the primary menus of android. We can use them for settings, search,
+ * delete item etc.
+ * <p>
+ * In This example we have inflated the menu by calling the inflate() method of MenuInflater class.
+ * To perform an even on each menu items, we override onOptionsItemSelected() method of the Activity
+ * class
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu. it adds items to the action bar if it's present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(getApplicationContext(), "Item 1 Selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(getApplicationContext(), "Item 2 Selected", Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.item3:
+                Toast.makeText(getApplicationContext(), "Item 3 Slected", Toast.LENGTH_LONG).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
