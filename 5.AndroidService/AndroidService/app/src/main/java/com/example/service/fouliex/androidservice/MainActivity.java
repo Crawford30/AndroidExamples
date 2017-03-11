14d7bfb07f83de77b01dfcf958ae6740637c3b45
George Fouche <fouliex@gmail.com>
2017-03-10 16:00:18 -0800

    Add comment for AndroidService

--- 5.AndroidService/AndroidService/app/src/main/java/com/example/service/fouliex/androidservice/MainActivity.java
+++ 5.AndroidService/AndroidService/app/src/main/java/com/example/service/fouliex/androidservice/MainActivity.java
@@ -3,6 +3,15 @@ package com.example.service.fouliex.androidservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Android Service is a component that is used to perform operations on the background,for example
+ * playing music,handle network transactions, interacting content providers etc.
+ * <p>
+ * It doesn't have any UI and the service runs in the background indefinitely even if the
+ * application is destroyed.
+ * <p>
+ * Service <- ContextWrapper <- Context <- Object.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
