2ded47876e68ba72041808b7cc9edd46afd1f15c
George Fouche <fouliex@gmail.com>
2017-03-16 16:11:54 -0700

    Android preferences WIP

--- 7.AndroidStorage/AndroidPreferences/app/src/main/java/com/example/preferences/fouliex/androidpreferences/MainActivity.java
+++ 7.AndroidStorage/AndroidPreferences/app/src/main/java/com/example/preferences/fouliex/androidpreferences/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.preferences.fouliex.androidpreferences;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button storeinforamtion = (Button) findViewById(R.id.storeinformation);
+        Button showinformation = (Button)findViewById(R.id.storeinformation);
+
     }
 }
