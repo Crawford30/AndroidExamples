bcdc8d6bd0f068e58a2e76b0f3a6033c6c7a4fe5
fouliex <fouliex@gmail.com>
2017-03-20 22:08:04 -0700

    Add Internal Storage Example

--- 7.AndroidStorage/AndroidInternalStorage/app/src/main/java/com/example/storage/internal/fouliex/androidinternalstorage/MainActivity.java
+++ 7.AndroidStorage/AndroidInternalStorage/app/src/main/java/com/example/storage/internal/fouliex/androidinternalstorage/MainActivity.java
@@ -1,13 +1,86 @@
 package com.example.storage.internal.fouliex.androidinternalstorage;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextFileName, editTextData;
+    Button saveButton, readButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextFileName = (EditText) findViewById(R.id.editText1);
+        editTextData = (EditText) findViewById(R.id.editText2);
+        saveButton = (Button) findViewById(R.id.button1);
+        readButton = (Button) findViewById(R.id.button2);
+
+        //Performing Action on Read Button
+        performActionOnSave();
+        //Performing Action on Save Button
+        performActionOnRead();
+
+    }
+
+    private void performActionOnRead() {
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String filename = editTextFileName.getText().toString();
+                StringBuffer stringBuffer = new StringBuffer();
+
+                try {
+                    BufferedReader inputReader = new BufferedReader(new InputStreamReader(openFileInput(filename)));
+                    String inputString;
+                    while ((inputString = inputReader.readLine()) != null) {
+                        stringBuffer.append(inputString + "\n");
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(getApplicationContext(), stringBuffer.toString(),
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    void performActionOnSave() {
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String filename = editTextFileName.getText().toString();
+                String data = editTextData.getText().toString();
+
+                FileOutputStream fos;
+
+                try {
+                    fos = openFileOutput(filename, Context.MODE_PRIVATE);
+                    fos.write(data.getBytes());
+                    fos.close();
+
+                    Toast.makeText(getApplicationContext(), filename + " saved", Toast.LENGTH_LONG).show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
